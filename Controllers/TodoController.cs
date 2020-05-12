c62231a4f204bcf1c5ebc658df853fbfac70152d
James Petersen <jpetersenames@gmail.com>
2020-05-12 18:05:53 +0000

    Converted to todo and returning json strings

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class TodoController : ControllerBase
+    {
+        private static readonly string[] UnfinishedTasks = new[]
+        {
+            "Feed the dogs", "Walk the dogs", "Finish demo"
+        };
+
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            TodoTasks[] todos = new TodoTasks[3];
+            for (int i = 0; i < 3; ++i)
+            {
+                todos[i] = new TodoTasks
+                {
+                    Status = "TODO",
+                    Task = UnfinishedTasks[i],
+                    Id = i
+                };
+            }
+
+            return JsonSerializer.Serialize(todos);
+        }
+    }
+}
---
+++ TodoTasks.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace todo
+{
+    public class TodoTasks
+    {
+        public string Status { get; set; }
+
+        public string Task { get; set; }
+
+        public int Id { get; set; }
+    }
+}
